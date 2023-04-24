34300868337a82f1a088289d11e73fdd194de52f
Pouria <pouria.msd@gmail.com>
2023-04-24 11:55:24 +0200

    Creating evaluate function

--- src/App.jsx
+++ src/App.jsx
@@ -49,6 +49,28 @@ function reducer(state, { type, payload }) {
   }
 }
 
+function evaluate({ currentOperand, previousOperand, operation }) {
+  const prev = parseFloat(previousOperand);
+  const curr = parseFloat(currentOperand);
+  if (isNaN(prev) || isNaN(curr)) return "";
+  let computation = "";
+  switch (operation) {
+    case "+":
+      computation = prev + curr;
+      break;
+    case "-":
+      computation = prev - curr;
+      break;
+    case "*":
+      computation = prev * curr;
+      break;
+    case "÷":
+      computation = prev / curr;
+      break;
+  }
+  return computation.toString();
+}
+
 function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
     reducer,
